d7205413ad0705ff0fff82391959f7fa9e7c84c2
jdvera <jdvera2015@gmail.com>
2018-05-05 16:15:36 -0500

    adding react boilerplate

--- models/users.js
+++ models/users.js
@@ -1,5 +1,3 @@
-var bcrypt = require("bcrypt-nodejs");
-
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
     username: {
@@ -9,62 +7,22 @@ module.exports = function(sequelize, DataTypes) {
         len: [1]
       }
     },
-    password: {
+    address: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
-    },
-    homeAddress: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    online: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    activeLocation: DataTypes.STRING
+    }
   });
 
-  Users.validatePassword = function(password, passwd, done, user) {
-    bcrypt.compare(password, passwd, function(err, isMatch) {
-      if (err) {
-        done(err, false);
+  Users.associate = function(models) {
+    Users.belongsTo(models.Groups, {
+      foreignKey: {
+        allowNull: false
       }
-      else if (isMatch) {
-        done(null, user);
-      }
-      else if (!isMatch) {
-        done(null, false);
-      }
-    })
-  };
-
-  Users.beforeCreate(function(user, options) {
-    return cryptPassword(user.password)
-      .then(success => {
-        user.password = success;
-      })
-      .catch(err => {
-        if (err) console.log(err);
-      });
-  });
-
-  function cryptPassword(password) {
-    return new Promise(function(resolve, reject) {
-      bcrypt.genSalt(10, function(err, salt) {
-        if (err) return reject(err);
-        bcrypt.hash(password, salt, null, function(err, hash) {
-          if (err) return reject(err);
-          return resolve(hash);
-        });
-      });
     });
-  }
+  };
 
   return Users;
 };
--- server.js
+++ server.js
@@ -1,30 +1,61 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var passport = require("passport");
-var session = require("express-session");
-var passportConfig = require("./config/passport.js")(passport);
+// Dependencies  ========================================
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3010;
+// Sets up the Express App  =============================
+const PORT = process.env.PORT || 3010;
+const app = express();
 
+// **Uncomment once we begin passport work**
+// Sets up Passport =====================================
+var passport   = require('passport');
+var session    = require('express-session');
+var flash = require('connect-flash');
+
+// Requiring our models for syncing
 var db = require("./models");
 
+
+// Sets up the Express app to han le data parsing -------/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(session({ secret: "goatjsformakingbettersecurity"}));
+
+// **Uncomment once we begin passport work**
+// For Passport -----------------------------------------/
+app.use(flash());
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
 app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static("public"));
-
-require("./routes/html-routes.js")(passport, app);
-require("./routes/api-routes.js")(app);
+app.use(passport.session()); // persistent login sessions
 
 
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Server Routes
+// =============================================================
+require("./routes/user-routes.js")(app, passport);
+require("./routes/shoesRoutes.js")(app);
+require("./routes/dressesRoutes.js")(app);
+// load passport strategies
+require('./config/passport.js')(passport);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+// add { force: true } in ".sync()" function if writing db schema for first time.
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+	  console.log(`🌎 ==> Server now on port ${PORT}!`);
+	});
 });
