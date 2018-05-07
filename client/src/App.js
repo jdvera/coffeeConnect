f876578dcdc0dd824fc220e64765faa906fc7604
jdvera <jdvera2015@gmail.com>
2018-05-07 10:14:39 -0500

    testing react display

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hi to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/main" component={Main} />
+				</Switch>
+			</Router>
     );
   }
 }
