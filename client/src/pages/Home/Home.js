2880a9f90fb8eef12f4d07aec7ec23c45aa24a5d
jdvera <jdvera2015@gmail.com>
2018-05-25 17:10:29 -0500

    index page now appearing

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,9 +4,38 @@ import "./Home.css";
 class Home extends Component {
     render(){
         return (
-            <div className="home-wrapper">
-                Home
-            </div>
+            <div className="container">
+			<div className="row">
+			</div>
+			
+
+			<div className="row">
+				<div>
+					<img src="./images/coffeelogoMed.png" alt="logo" id="imgStyle" />
+				</div>
+			</div>
+			<div className="row">
+				<div className="" id="fontStyle">coffee</div>
+				<div className="" id="fontStyle2">connection</div>
+			</div>
+			
+			<div className="row">
+				<div className="">
+					<div className="row">
+						<a href="welcome"><button>log in</button></a>
+					</div>
+				</div>
+
+			</div>
+
+			<div className="row">
+				<div className="">
+					<div className="row">
+						<a href="register"><button>new user</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
         );
     }
 }
